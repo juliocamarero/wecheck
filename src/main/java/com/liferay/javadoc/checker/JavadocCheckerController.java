ec2f17dac35c5b3539902c8e8932ae1773c98eba
Julio Camarero <julio.camarero@liferay.com>
2017-02-09 17:55:35 +0100

    Use system variables for github user and key

--- src/main/java/com/liferay/javadoc/checker/JavadocCheckerController.java
+++ src/main/java/com/liferay/javadoc/checker/JavadocCheckerController.java
@@ -21,10 +21,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Random;
+import java.util.logging.Logger;
+
 import org.apache.commons.io.FileUtils;
+
+import javax.xml.bind.DatatypeConverter;
 
 @Controller
 @RequestMapping("/github")
@@ -41,7 +51,7 @@ public class JavadocCheckerController {
 	public String service(@RequestBody GithubMessage githubMessage)
 		throws JSONException, IOException, InterruptedException {
 
-		System.out.println("Message from Github Received");
+		LOGGER.info("A Message from Github was Received");
 
 		if (githubMessage.isOpen()) {
 		    GithubPullRequest pullRequest = githubMessage.getPull_request();
@@ -56,77 +66,188 @@ public class JavadocCheckerController {
 
 			String repoFullName = repo.getFull_name();
 
-			System.out.println(
+			LOGGER.info(
 				"Pull Request from " + repoFullName + " - Number " + number);
 
 		    JSONObject data = new JSONObject();
 
 		    data.put("body", "Checking JavaDocs...");
 
-		    StringBuilder curl = new StringBuilder(5);
+			tryToPostMessage(
+				_MAX_RETRIES_DEFAULT, repoFullName, number, data.toString());
 
-		    curl.append("https://api.github.com/repos/");
-		    curl.append(repoFullName);
-		    curl.append("/issues/");
-		    curl.append(number);
-		    curl.append("/comments");
+		    String message = executeJavadocsChecker(repoFullName, ref);
 
-			System.out.println("Posting a message to the pull request: ");
-
-		    ProcessBuilder pb = new ProcessBuilder(
-		        "curl", "-u", "lfr-checkstyle:c245230f8c463ad7284bb6d004d2cdae662411cc",
-		        curl.toString(), "--data", data.toString());
-
-		    Runtime runtime = Runtime.getRuntime();
-
-		    Process start = pb.start();
-
-		    start.waitFor();
-
-		    System.out.println(curl.toString());
-
-		    Random random = new Random();
-
-		    String folderName = ref + String.valueOf(random.nextLong());
-
-			StringBuilder sb = new StringBuilder(6);
-
-		    sb.append("git clone https://github.com/");
-		    sb.append(repoFullName);
-		    sb.append(" -b ");
-		    sb.append(ref);
-		    sb.append(" /tmp/");
-		    sb.append(folderName);
-
-		    Process clone = runtime.exec(sb.toString());
-
-		    clone.waitFor();
-
-		    File dir = new File("/tmp", folderName);
-
-		    Process build = runtime.exec("/opt/gradle-3.0/bin/gradle checkstyle", new String[]{}, dir);
-
-		    build.waitFor();
-			
-		    File reportFile = new File(dir, "checkstyle_report.html");
-
-		    data = new JSONObject();
-
-		    data.put("body", FileUtils.readFileToString(new File(dir, "checkstyle_report.html")));
-
-		    pb = new ProcessBuilder(
-		        "curl", "-u", "lfr-checkstyle:c245230f8c463ad7284bb6d004d2cdae662411cc",
-		        curl.toString(), "--data", data.toString());
-
-		    Process comment = pb.start();
-
-		    comment.waitFor();
-
-		    FileUtils.deleteDirectory(dir);
-
+			tryToPostMessage(
+				_MAX_RETRIES_DEFAULT, repoFullName, number, message);
 		}
 
 		return "SUCCESS";
 	}
+
+	private String executeJavadocsChecker(String repoFullName, String ref)
+		throws IOException, InterruptedException, JSONException {
+
+		Random random = new Random();
+
+		String folderName = ref + String.valueOf(random.nextLong());
+
+		StringBuilder sb = new StringBuilder(6);
+
+		String projectDir = "/tmp/" + folderName;
+
+		sb.append("git clone https://github.com/");
+		sb.append(repoFullName);
+		sb.append(" -b ");
+		sb.append(ref);
+		sb.append(" ");
+		sb.append(projectDir);
+
+		LOGGER.info(sb.toString());
+
+		ProcessBuilder gitClone = new ProcessBuilder(sb.toString().split(" "));
+
+		gitClone.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+		gitClone.redirectError(ProcessBuilder.Redirect.INHERIT);
+
+		gitClone.start().waitFor();
+
+		File dir = new File(projectDir);
+
+
+		ProcessBuilder build = new ProcessBuilder(
+			projectDir + "/gradlew", "-p", projectDir, "checkstyle");
+
+		build.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+		build.redirectError(ProcessBuilder.Redirect.INHERIT);
+
+		build.start().waitFor();
+
+		File reportFile = new File(dir, "checkstyle_report.html");
+
+		JSONObject data = new JSONObject();
+
+		data.put(
+			"body",
+			FileUtils.readFileToString(reportFile, Charset.defaultCharset()));
+
+		FileUtils.deleteDirectory(dir);
+
+		return data.toString();
+	}
+
+	private void tryToPostMessage(
+			int maxRetries, String repoFullName, String number, String message)
+		throws IOException {
+
+		int retryCount = 0;
+
+		try {
+			postMessage(repoFullName, number, message);
+		}
+		catch (IOException ioe) {
+			LOGGER.warning("Error when posting comment in github.");
+
+			retryCount++;
+
+			if ((maxRetries >= 0) && (retryCount >= maxRetries)) {
+				throw ioe;
+			}
+
+			LOGGER.warning(
+				"Retrying in " + _RETRY_PERIOD_DEFAULT + " seconds. (" +
+				retryCount + ")");
+
+			sleep(1000 * _RETRY_PERIOD_DEFAULT);
+		}
+	}
+
+	public static void sleep(long duration) {
+		try {
+			Thread.sleep(duration);
+		}
+		catch (InterruptedException ie) {
+			throw new RuntimeException(ie);
+		}
+	}
+
+	private String postMessage(String repoFullName, String number, String message)
+		throws IOException {
+
+		StringBuilder url = new StringBuilder(5);
+
+		url.append("https://api.github.com/repos/");
+		url.append(repoFullName);
+		url.append("/issues/");
+		url.append(number);
+		url.append("/comments");
+
+		URL urlObject = new URL(url.toString());
+
+		HttpURLConnection connection =
+			(HttpURLConnection)urlObject.openConnection();
+
+		connection.setRequestMethod("POST");
+
+		String githubUser = System.getenv("githubUser");
+		String githubKey = System.getenv("githubKey");
+
+		String userpass = githubUser + ":" + githubKey;
+		String basicAuth = "Basic " +
+			DatatypeConverter.printBase64Binary(userpass.getBytes("UTF-8"));
+
+		connection.setRequestProperty(
+			"Authorization", basicAuth);
+
+		connection.setRequestProperty(
+			"Content-Type", "application/json");
+
+		connection.setDoOutput(true);
+		
+		OutputStreamWriter out = new OutputStreamWriter(
+			connection.getOutputStream());
+
+		out.write(message);
+	    out.close();
+
+		int bytes = 0;
+		String line = null;
+
+		StringBuilder sb = new StringBuilder();
+
+		try (BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(
+					connection.getInputStream()))) {
+
+			while ((line = bufferedReader.readLine()) != null) {
+				byte[] lineBytes = line.getBytes();
+
+				bytes += lineBytes.length;
+
+				if (bytes > (30 * 1024 * 1024)) {
+					sb.append("Response for ");
+					sb.append(url);
+					sb.append(" was truncated due to its size.");
+
+					break;
+				}
+
+				sb.append(line);
+				sb.append("\n");
+			}
+		}
+
+		String response = sb.toString();
+
+		LOGGER.info("Comment posted successfully to Github");
+
+		return response;
+	}
+
+	private static final int _RETRY_PERIOD_DEFAULT = 5;
+	private static final int _MAX_RETRIES_DEFAULT = 3;
+
+	private final static Logger LOGGER = Logger.getLogger(
+		JavadocCheckerController.class.getName());
 
 }
