d80383462f332318a9d770d4de4a3f6956e32132
Julio Camarero <julio.camarero@liferay.com>
2017-02-24 20:19:01 +0100

    Add a configuration reader to read the properties from the project

---
+++ src/main/java/com/liferay/javadoc/checker/configuration/JavadocCheckerConfigurationReader.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.javadoc.checker.configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.Properties;
+import java.util.logging.Logger;
+
+/**
+ * @author Julio Camarero
+ */
+public class JavadocCheckerConfigurationReader {
+
+	public JavadocCheckerConfigurationReader(String projectDirectory) {
+		_projectDirectory = projectDirectory;
+
+		if (!_projectDirectory.endsWith("/")) {
+			_projectDirectory = projectDirectory.concat("/");
+		}
+
+		_properties = _loadProperties(_projectDirectory + _propertiesFile);
+	}
+
+	public String[] getExcludeDirectories() {
+		String[] excludeDirectories = new String[0];
+
+		if (_properties != null) {
+			String value = _properties.getProperty("exclude.directories");
+
+			if ((value != null) && !value.isEmpty()) {
+				String[] relativeDirectories = value.split(",");
+
+				excludeDirectories = new String[relativeDirectories.length];
+
+				for (int i = 0; i < relativeDirectories.length; i++) {
+					excludeDirectories[i] =
+						_projectDirectory + relativeDirectories[i];
+				}
+			}
+		}
+
+		return excludeDirectories;
+	}
+
+	public String[] getIncludeDirectories() {
+		String[] includeDirectories = new String[] {"."};
+
+		if (_properties != null) {
+			String value = _properties.getProperty("include.directories");
+
+			if ((value != null) && !value.isEmpty()) {
+				String[] relativeDirectories = value.split(",");
+				includeDirectories = new String[relativeDirectories.length];
+
+				for (int i = 0; i < relativeDirectories.length; i++) {
+					includeDirectories[i] =
+						_projectDirectory + relativeDirectories[i];
+				}
+			}
+		}
+
+		return includeDirectories;
+	}
+
+	public String getReportTitle() {
+		String reportTitle = null;
+
+		if (_properties != null) {
+			reportTitle = _properties.getProperty("report.title");
+		}
+
+		if (reportTitle != null && !reportTitle.isEmpty()) {
+			reportTitle = _defaultReportTitle;
+		}
+
+		return reportTitle;
+	}
+
+	private Properties _loadProperties(String propertiesFile) {
+		Properties properties = new Properties();
+
+		InputStream inputStream = null;
+
+		try {
+			inputStream = new FileInputStream(propertiesFile);
+
+			properties.load(inputStream);
+		}
+		catch (IOException ioe) {
+			LOGGER.warning("Unable to load properties file: " + ioe);
+
+			ioe.printStackTrace();
+		}
+		finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				}
+				catch (IOException ioe) {
+					LOGGER.warning(
+						"Unable to close inputstream for reading properties: " +
+						ioe);
+
+					ioe.printStackTrace();
+				}
+			}
+		}
+
+		return properties;
+	}
+
+	private static final Logger LOGGER = Logger.getLogger(
+		JavadocCheckerConfigurationReader.class.getName());
+
+	private String _defaultReportTitle = "";
+	private String _projectDirectory;
+	private Properties _properties;
+	private String _propertiesFile = "javadocChecker.properties";
+
+}
