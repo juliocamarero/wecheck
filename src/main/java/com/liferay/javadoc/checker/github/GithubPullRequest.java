44da844576d2ab66fc18f841b1d587d1a599ca24
Julio Camarero <julio.camarero@liferay.com>
2017-02-27 10:28:58 +0100

    Read number from pull request object

--- src/main/java/com/liferay/javadoc/checker/github/GithubPullRequest.java
+++ src/main/java/com/liferay/javadoc/checker/github/GithubPullRequest.java
@@ -1,16 +1,17 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.javadoc.checker.github;
 
 /**
@@ -22,6 +23,10 @@ public class GithubPullRequest {
 		return _head;
 	}
 
+	public String getNumber() {
+		return _number;
+	}
+
 	public String getRef() {
 		return _ref;
 	}
@@ -30,11 +35,16 @@ public class GithubPullRequest {
 		_head = head;
 	}
 
+	public void setNumber(String number) {
+		_number = number;
+	}
+
 	public void setRef(String ref) {
 		_ref = ref;
 	}
 
 	private GithubPullRequestHead _head;
+	private String _number;
 	private String _ref;
 
 }
