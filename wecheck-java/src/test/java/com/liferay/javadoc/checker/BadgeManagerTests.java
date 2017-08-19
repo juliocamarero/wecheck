9ca9151e41ae53a3daaf5dec0860b8879f805a8a
Julio Camarero <julio.camarero@liferay.com>
2017-08-19 18:08:36 +0200

    Add test for badge Manager

---
+++ wecheck-java/src/test/java/com/liferay/javadoc/checker/BadgeManagerTests.java
@@ -0,0 +1,25 @@
+package com.liferay.javadoc.checker;
+
+import com.liferay.javadoc.checker.processor.BadgeManager;
+import org.json.JSONException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BadgeManagerTests {
+
+	@Test
+	public void buildParser() throws JSONException {
+		Assert.assertEquals(
+			"https://img.shields.io/badge/javadoc-20.00%-red.svg", _badgeManager.getBadgeURL(20));
+	}
+
+	@Autowired
+	private BadgeManager _badgeManager;
+
+}
